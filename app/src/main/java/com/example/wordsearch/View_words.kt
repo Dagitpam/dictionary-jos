1d894322d94e65a1cebf99641306d0925b476774
Your Name <nyam4real>
2019-09-27 15:50:03 +0000

    delete module added

--- app/src/main/java/com/example/wordsearch/View_words.kt
+++ app/src/main/java/com/example/wordsearch/View_words.kt
@@ -1,8 +1,14 @@
 package com.example.wordsearch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.wordsearch.helpers.DatabaseHelper
+import com.example.wordsearch.model.Words
 
 class View_words : AppCompatActivity() {
     private  lateinit var title : TextView
@@ -24,5 +30,26 @@ class View_words : AppCompatActivity() {
         title.setText(title_word)
 
         meaning.setText(meaning_word)
+
+        //Delete and update user
+
+        val delete_btn = findViewById<Button>(R.id.bt_view_word_delete)
+        delete_btn.setOnClickListener(View.OnClickListener {
+            //create a db instance
+            val db_helper = DatabaseHelper(this)
+
+            val word = Words(id = id_word, title = "", meaning = "")
+            db_helper.deleteWord(word)
+            //Redirect the user to main activity
+
+            val toMain = Intent(this,MainActivity::class.java)
+            toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            toMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            toMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(toMain)
+            finish()
+
+            Toast.makeText(this, "Word deleted successfully", Toast.LENGTH_LONG).show()
+        })
     }
 }
